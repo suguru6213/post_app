0d212b643d0cc7fc8018828da7b0291e5c266568
suguru6213 <t6v2x1q3@gmail.com>
2020-05-20 19:15:29 +0900

    update_posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,33 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(post_params)
+    # (image: params[:image], text: params[:text])
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+  def post_params
+    params.permit(:image, :text)
+  end
+
+
 end
