c0394a3669456becdd55bddcc5cdad461ca79bca
suguru6213 <t6v2x1q3@gmail.com>
2020-05-20 20:32:11 +0900

    add_mypage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,18 @@
 class PostsController < ApplicationController
-
+   before_action :move_to_index,  except: :index
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Post.create(post_params)
-    # (image: params[:image], text: params[:text])
+    Post.create(image: post_params[:image], text: post_params[:text], user_id: current_user.id)
   end
 
   def edit
+
   end
 
   def show
@@ -29,5 +29,7 @@ class PostsController < ApplicationController
     params.permit(:image, :text)
   end
 
-
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @name = current_user.name
+    @posts = current_user.posts.page(params[:page]).per(5).order("created_at DESC")
   end
 end
