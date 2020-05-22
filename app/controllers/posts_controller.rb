3c243b6f098e383e245c160a3995e9c19fefc993
suguru6213 <t6v2x1q3@gmail.com>
2020-05-23 01:21:39 +0900

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
    before_action :move_to_index,  except: [:index, :show]
   def index
     @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+      
   end
 
   def new
